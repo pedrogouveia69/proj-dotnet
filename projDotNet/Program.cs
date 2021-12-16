01ad7e78ac527d9c0b1f777faa401153fcda583c
Pedro Gouveia <58569642+pedrogouveia69@users.noreply.github.com>
2021-12-16 15:49:07 +0000

    first commit

---
+++ projDotNet/Program.cs
@@ -0,0 +1,9 @@
+﻿void buildMenu(string title, string[] options) 
+{
+    Console.WriteLine(title);
+    for (int i = 0; i < options.Length; i++) {
+        Console.WriteLine(i + 1 + " - " + options[i]);
+    } 
+}
+
+buildMenu("Bem-vindo", new string[] { "Opção 1", "Opção 2" });
